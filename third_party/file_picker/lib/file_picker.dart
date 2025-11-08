6835bc4d2419e4af54533ee9c78c10726b86b0aa
L-Hall <L-Hall@users.noreply.github.com>
2025-11-08 22:24:22 +0000

    ios and macos builds

--- third_party/file_picker/lib/file_picker.dart
+++ third_party/file_picker/lib/file_picker.dart
@@ -3,3 +3,6 @@ library file_picker;
 export './src/file_picker.dart';
 export './src/platform_file.dart';
 export './src/file_picker_result.dart';
+export './src/file_picker_macos.dart' show FilePickerMacOS;
+export './src/linux/file_picker_linux.dart' show FilePickerLinux;
+export './src/windows/file_picker_windows.dart' show FilePickerWindows;
