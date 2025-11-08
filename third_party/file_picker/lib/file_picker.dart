6dd47e5bfc9e28f53d15df8fda239444232e98be
L-Hall <L-Hall@users.noreply.github.com>
2025-11-08 23:09:08 +0000

    fixed chrome build

--- third_party/file_picker/lib/file_picker.dart
+++ third_party/file_picker/lib/file_picker.dart
@@ -5,4 +5,6 @@ export './src/platform_file.dart';
 export './src/file_picker_result.dart';
 export './src/file_picker_macos.dart' show FilePickerMacOS;
 export './src/linux/file_picker_linux.dart' show FilePickerLinux;
-export './src/windows/file_picker_windows.dart' show FilePickerWindows;
+export './src/windows/stub.dart'
+    if (dart.library.io) './src/windows/file_picker_windows.dart'
+    show FilePickerWindows;
--- third_party/file_picker/lib/src/windows/stub.dart
+++ third_party/file_picker/lib/src/windows/stub.dart
@@ -1,4 +1,13 @@
 import 'package:file_picker/file_picker.dart';
 
-/// Stub method to support both dart:ffi and web
-FilePicker filePickerWithFFI() => throw UnimplementedError('Unsupported');
+/// Placeholder implementation used on platforms where `dart:ffi` is not
+/// available (for example, Flutter Web). All operations throw by default to
+/// match the behaviour of the real Windows implementation when it cannot
+/// operate.
+class FilePickerWindows extends FilePicker {
+  FilePickerWindows();
+}
+
+/// Stub method to support both dart:ffi and web.
+FilePicker filePickerWithFFI() =>
+    throw UnsupportedError('Windows file picker is not available.');
